053e5b3d6a54f742912bdd8eb985121222f25f7a
Auro <aurokumar926@gmail.com>
2020-03-27 13:11:15 +0530

    no maj change

--- tij2_ch3_main/EverythingIsAnObject_main.java
+++ tij2_ch3_main/EverythingIsAnObject_main.java
@@ -1,7 +1,6 @@
 package tij2_ch3_main;
 
 import tij2_ch3.EverythingIsAnObject;
-import tij2_ch3.EverythingIsAnObject.*;
 
 class EverythingIsAnObject_main {
     public static void main(String[] args) {
