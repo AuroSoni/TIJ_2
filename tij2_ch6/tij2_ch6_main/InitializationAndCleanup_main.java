5e4b6c7cb2a2092acd10475168ffe8ba101bb291
Auro <aurokumar926@gmail.com>
2020-03-30 10:01:48 +0530

    Exercise 10 added,...,...

--- tij2_ch6/tij2_ch6_main/InitializationAndCleanup_main.java
+++ tij2_ch6/tij2_ch6_main/InitializationAndCleanup_main.java
@@ -14,9 +14,13 @@ public class InitializationAndCleanup_main{
     public static void main(String[] args) {
         InitializationAndCleanup obj = new InitializationAndCleanup();
         
+        System.out.println("Exercise 10 is similar to Exercise 11. ");
+        System.out.println("Running Exercise 11 : ");
         Exercise11 obj11=obj.exercise11(10);
         obj.exercise11(11);
         obj11.toZero();
         System.gc();
+        System.runFinalization();//Calling System.gc( ) and System.runFinalization( ) in sequence will probably 
+                                 //but not necessarily call your finalizer (The behavior of finalize has been uncertain from one version of JDK to another.) 
     }
 }
