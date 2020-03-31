b19221cc06551eb6f436ada5ba777d4c131e7e4a
Auro <aurokumar926@gmail.com>
2020-03-31 20:08:00 +0530

    Variable argument lists tested. Imp.

---
+++ Experiments/Varargs.java
@@ -0,0 +1,18 @@
+public class Varargs {   
+    static void f(Character... args) {     
+        System.out.print(args.getClass());     
+        System.out.println(" length " + args.length);   
+    }   
+    static void g(int... args) {     
+        System.out.print(args.getClass());     
+        System.out.println(" length " + args.length);   
+    }   
+    public static void main(String[] args) {  
+        f('a',Character.valueOf(' '));     
+        f();     
+        g(1,2,5);     
+        g();
+        g((int[])new int[]{1,2});     
+        System.out.println("int[]: " + new int[0].getClass());   
+    } 
+}
