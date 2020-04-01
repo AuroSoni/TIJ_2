d6872575425188b99240630ad64c9aad8a284244
Auro <aurokumar926@gmail.com>
2020-04-01 09:11:43 +0530

    Access control resolved

--- Experiments/access_control/access_control_other/Importing_other.java
+++ Experiments/access_control/access_control_other/Importing_other.java
@@ -7,6 +7,6 @@ public class Importing_other{
         PackageAccess.main(args);
         access_control.access.Importing.main(args);
         int i=new PackageAccess().public_pa;
-        new PackageAccess().pr("Dangling obj reference  "+ i);
+        new PackageAccess().pr("Dangling obj reference   "+ i);
     }
 }
