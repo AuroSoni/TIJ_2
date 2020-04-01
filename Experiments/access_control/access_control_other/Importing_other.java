2ea52e7c92151ac93d2d8f607eba3c967a389a3a
Auro <aurokumar926@gmail.com>
2020-04-01 09:00:36 +0530

    Resolves all package control issues

---
+++ Experiments/access_control/access_control_other/Importing_other.java
@@ -0,0 +1,12 @@
+package access_control_other;
+
+import access_control.PackageAccess;
+
+public class Importing_other{
+    public static void main(String[] args) {
+        PackageAccess.main(args);
+        access_control.access.Importing.main(args);
+        int i=new PackageAccess().public_pa;
+        new PackageAccess().pr("Dangling obj reference  "+ i);
+    }
+}
