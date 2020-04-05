bd0b35ea4e44e397099f4ee314bc2a9653a38e47
Auro <aurokumar926@gmail.com>
2020-04-05 10:59:32 +0530

    Exercise 1 created, running successfully

--- tij2_ch9/tij2_ch9/ex1/Exercise1.java
+++ tij2_ch9/tij2_ch9/ex1/Exercise1.java
@@ -1,19 +1,57 @@
 package tij2_ch9.tij2_ch9.ex1;
+import static custom_tools.tools.print.Print.print;
 
 enum Level{
     NOOB,BEGINNER,INTERMEDIATE,PROFESSIONAL,GOD;
 }
 
 class Cycle{
-    void ride(Level l){
-        
+    void level(Level l){
+        print("Cycle.level(): "+l);
+    }
+}
+
+class Unicycle extends Cycle{
+    @Override
+    void level(Level l){
+        print("Unicycle.level(): "+l);
+    }
+}
+
+class Bicycle extends Cycle{
+    @Override
+    void level(Level l){
+        print("Bicycle.level(): "+l);
+    }
+}
+
+class Tricycle extends Cycle{
+    @Override
+    void level(Level l){
+        print("Tricycle.level(): "+l);
     }
 }
 
 /**
  * Create a Cycle class, with subclasses Unicycle, Bicycle and Tricycle. 
- * Demonstrate that an instance of each type can be upcast to Cycle via a ride( ) method.
+ * Demonstrate that an instance of each type can be upcast to Cycle via a level( ) method.
  */
 public class Exercise1{
 
+    public static void ride(Cycle c){
+        c.level(Level.NOOB);
+    }
+
+    public static void main(String[] args) {
+        Tricycle t=new Tricycle();
+        Bicycle b=new Bicycle();
+        Unicycle u=new Unicycle();
+    
+        print("Upcasting Unicycle : ");
+        ride(u);
+        print("Upcasting Bicycle : ");
+        ride(b);
+        print("Upcasting Tricycle : ");
+        ride(t);
+    }
 }
--- tij2_ch9/tij2_ch9_main/Polymorphism.java
+++ tij2_ch9/tij2_ch9_main/Polymorphism.java
@@ -1,5 +1,12 @@
 package tij2_ch9.tij2_ch9_main;
+import static custom_tools.tools.print.Print.print;
+
+import tij2_ch9.tij2_ch9.ex1.Exercise1;
 
 public class Polymorphism {
-
+    public static void main(String[] args) {
+        
+        print("Running Exercise 1 : ");
+        Exercise1.main(args);
+    }
 }
