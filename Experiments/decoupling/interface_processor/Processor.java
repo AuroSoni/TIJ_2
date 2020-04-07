5a8ed23b1411055308a0c44322fde20ed6fa40af
Auro <aurokumar926@gmail.com>
2020-04-07 11:15:58 +0530

    decoupling explained

--- Experiments/decoupling/interface_processor/Processor.java
+++ Experiments/decoupling/interface_processor/Processor.java
@@ -8,7 +8,14 @@ package experiments.decoupling.interface_processor;
  * extending the implementation to several derived classes so as to utilise polymorphism.
  * Why not just create several implementations of {@link Processor} and override them to 
  * utilise polymorphism ? It could have been done. But here the former is more appropriate.
+ * 
+ * The User who has just discovered this library and wants to utilise {@link Processor}:
+ * He would first have to create an apdapter for his class(using delegation) that implements 
+ * the proccesor interface.
+ * 
  * @see experiments.decoupling.interface_processor.StringProcessor
+ * @see experiments.decoupling.interface_processor.FilterAdapter
+ * @see experiments.decoupling.interface_processor.Apply
  */
 public interface Processor {   
     String name();   
