c4fc5152f99a73130a1269756ce1e650c7972f56
Auro <aurokumar926@gmail.com>
2020-04-08 19:00:40 +0530

    ex 7, 8 created, RS

---
+++ tij2_ch11/tij2_ch11/Exercise7.java
@@ -0,0 +1,40 @@
+package tij2_ch11.tij2_ch11;
+import static custom_tools.tools.print.Print.print;
+
+class Outer{
+    private int field;
+    private void say(){
+        print("Outer.say()");
+    }
+    class Inner{
+        private int ref=0;
+        private Inner(){
+            print("Creating Inner Object");
+        }
+        private void modify(int i){
+            field=i;
+            print("Inner.modify(): "+field,"Calling say().");
+            say();
+        }
+    }
+    void call(int j){
+        //! ref++;
+        print("Outer.call()");
+        Inner i=new Inner();
+        i.modify(j);
+        i.ref++;     //Outer class has access to all private fields and methods of Inner class.
+    }
+}
+
+/**
+ * Create a class with a private field and a private method. 
+ * Create an inner class with a method that modifies the outer-class field and calls the outer-class method. 
+ * In a second outer-class method, create an object of the inner class and call its method, 
+ * then show the effect on the outer-class object. 
+ */
+public class Exercise7 {
+    public static void main(String[] args) {
+        Outer o= new Outer();
+        o.call(5);
+    }
+}
--- tij2_ch11/tij2_ch11_main/InnerClasses.java
+++ tij2_ch11/tij2_ch11_main/InnerClasses.java
@@ -2,9 +2,11 @@ package tij2_ch11.tij2_ch11_main;
 
 import static custom_tools.tools.print.Print.print;
 
+import tij2_ch11.tij2_ch11.Exercise7;
 import tij2_ch11.tij2_ch11.ex1.Exercise1;
 import tij2_ch11.tij2_ch11.ex2.Exercise2;
 import tij2_ch11.tij2_ch11.ex3.Exercise3;
+import tij2_ch11.tij2_ch11.ex6.Exercise6;
 
 public class InnerClasses {
     public static void main(String[] args) {
@@ -16,5 +18,11 @@ public class InnerClasses {
 
         print("\nRunning Exercise 3 & 5: ");
         Exercise3.main(args);
+
+        print("\nRunning Exercise 6: ");
+        Exercise6.main(args);
+
+        print("\nRunning Exercise 7 & 8: ");
+        Exercise7.main(args);
     }
 }
