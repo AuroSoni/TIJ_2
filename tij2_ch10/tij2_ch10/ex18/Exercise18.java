d588743e56bcdb2eced6624708197e452ac88c8e
Auro <aurokumar926@gmail.com>
2020-04-08 10:25:22 +0530

    ex18 created, RS

---
+++ tij2_ch10/tij2_ch10/ex18/Exercise18.java
@@ -0,0 +1,62 @@
+package tij2_ch10.tij2_ch10.ex18;
+import static custom_tools.tools.print.Print.print;
+
+import java.util.Random;
+
+interface Chance{
+    public void chance();
+}
+
+interface ChanceFactory{
+    public Chance generate();
+}
+
+class Toss implements Chance {
+    @Override
+    public void chance() {
+        Random rand = new Random();
+        boolean result = rand.nextBoolean();
+        if (result)
+            print("TOSS : Heads");
+        else
+            print("TOSS : Tails");
+    }
+}
+
+class Roll implements Chance {
+    @Override
+    public void chance() {
+        Random rand = new Random();
+        int result = rand.nextInt(6) + 1;
+        print("ROLL : " + result);
+    }
+}
+
+class RollFactory implements ChanceFactory {
+    public Roll generate() {
+        return new Roll();
+    }
+}
+
+class TossFactory implements ChanceFactory {
+    public Toss generate() {
+        return new Toss();
+    }
+}
+
+/**
+ * Create a framework using Factory Methods that performs both coin tossing and
+ * dice tossing.
+ */
+public class Exercise18 {
+    public static void choose(ChanceFactory chanceFactory) {
+        Chance ch = chanceFactory.generate();
+        ch.chance();
+    }
+    public static void main(String[] args) {
+        print("Tossing : ");
+        choose(new TossFactory());
+        print("Rolling : ");
+        choose(new RollFactory());
+    }
+}
