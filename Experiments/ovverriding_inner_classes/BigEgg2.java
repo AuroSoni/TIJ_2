9b64bbf891b58941f7a6c36a11f12b47f5b03075
Auro <aurokumar926@gmail.com>
2020-04-10 19:07:50 +0530

    practising overriding inner classes

---
+++ Experiments/ovverriding_inner_classes/BigEgg2.java
@@ -0,0 +1,41 @@
+package experiments.ovverriding_inner_classes;
+import static custom_tools.tools.print.Print.*;
+
+class Egg2 {   
+    protected class Yolk {     
+        public Yolk() { 
+            print("Egg2.Yolk()"); 
+        }     
+        public void f() { 
+            print("Egg2.Yolk.f()");
+        }   
+    }   
+    private Yolk y = new Yolk();   
+    public Egg2() { 
+        print("New Egg2()"); 
+    }   
+    public void insertYolk(Yolk yy) { 
+        y = yy; 
+    }   
+    public void g() { 
+        y.f(); 
+    } 
+}  
+ 
+public class BigEgg2 extends Egg2 {   
+    public class Yolk extends Egg2.Yolk {     
+        public Yolk() { 
+            print("BigEgg2.Yolk()"); 
+        }     
+        public void f() { 
+            print("BigEgg2.Yolk.f()"); 
+        }   
+    }   
+    public BigEgg2() { 
+        insertYolk(new Yolk()); 
+    }   
+    public static void main(String[] args) {     
+        Egg2 e2 = new BigEgg2();     
+        e2.g();   
+    } 
+}
