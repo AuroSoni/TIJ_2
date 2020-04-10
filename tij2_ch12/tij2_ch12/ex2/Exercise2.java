d4a865bb002cdd3fab8c0f140bde72fa9d80dd82
Auro <aurokumar926@gmail.com>
2020-04-10 20:20:30 +0530

    ex2 created,RS

---
+++ tij2_ch12/tij2_ch12/ex2/Exercise2.java
@@ -0,0 +1,19 @@
+package tij2_ch12.tij2_ch12.ex2;
+import static custom_tools.tools.print.Print.*;
+import java.util.*;
+
+/**
+ * Modify SimpleCollection.java(aka Exercise2) to use a Set for c. 
+ */
+public class Exercise2 {
+    public static void main(String[] args) {
+        Collection<Integer> set=new HashSet<>();
+        for(int i=0;i<10;i++){
+            print("Add "+i+" : "+set.add(i));
+        }
+        print("Add "+9+" : "+set.add(9));
+        for (Integer integer : set) {
+            printnb(integer," ");
+        }
+    }
+}
--- tij2_ch12/tij2_ch12_main/HoldingYourObjects.java
+++ tij2_ch12/tij2_ch12_main/HoldingYourObjects.java
@@ -2,6 +2,7 @@ package tij2_ch12.tij2_ch12_main;
 import static custom_tools.tools.print.Print.*;
 
 import tij2_ch12.tij2_ch12.ex1.Exercise1;
+import tij2_ch12.tij2_ch12.ex2.Exercise2;
 import tij2_ch12.tij2_ch12.ex3.Exercise3;
 
 public class HoldingYourObjects {
@@ -9,6 +10,9 @@ public class HoldingYourObjects {
 
         print("Running Exercise 1 : ");
         Exercise1.main(args);
+
+        print("\nRunning Exercise 2 : ");
+        Exercise2.main(args);
 
         print("\nRunning Exercise 3 : ");
         Exercise3.main(args);
