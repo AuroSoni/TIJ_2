6e9a3c479ffd5b64e88d970fb166fee941a587b1
Auro <aurokumar926@gmail.com>
2020-04-14 08:29:58 +0530

    ex16 modified to include ex20

--- tij2_ch12/tij2_ch12/ex16/Exercise16.java
+++ tij2_ch12/tij2_ch12/ex16/Exercise16.java
@@ -1,9 +1,8 @@
 package tij2_ch12.tij2_ch12.ex16;
 
 import static custom_tools.print.Print.*;
-
+import custom_tools.map_utility.*;
 import java.util.*;
-
 import custom_tools.myio.TextFile;
 
 /**
@@ -11,11 +10,18 @@ import custom_tools.myio.TextFile;
  * count and display the number of vowels in each input word, 
  * and also display the total number of vowels in the input file.  
  */
-public class Exercise16 {
+public class Exercise16 extends MapUtility<Character,Integer>{
     private static long total_vowels=0;
     public static void main(String[] args) {
+        MapUtility<Character,Integer> mu=new MapUtility<Character,Integer>() {
+        };
+        
         HashSet<Character> vowels=new HashSet<>();
         Collections.addAll(vowels,'a','e','i','o','u','A','E','I','O','U');
+        /**
+         * (Ex20)Modify Exercise 16 so that you keep a count of the occurrence of each vowel.
+         */
+        LinkedHashMap<Character,Integer> vowelCount=mu.fill(vowels, Arrays.asList(0,0,0,0,0,0,0,0,0,0));
         HashSet<String> words=new HashSet<>();
         for (String word : new TextFile("tij2_ch12/tij2_ch12/Exercise16.java","\\W+"))  {
             if(!words.contains(word)){
@@ -24,6 +30,8 @@ public class Exercise16 {
                 for(char c : word.toCharArray()){
                     if(vowels.contains(c)){
                         vowels_in_word++;
+                        int i = vowelCount.get(c);
+                        vowelCount.put(c, ++i);
                     }
                 }
                 words.add(word);
@@ -33,5 +41,7 @@ public class Exercise16 {
             }
         }
         print("Total Vowels : "+total_vowels);
+        print("Vowel Count : ");
+        print(vowelCount);
     }
 }
