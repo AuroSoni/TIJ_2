8a550994616e2fc54f7e49faff5b1e6f8aa856f4
Auro <aurokumar926@gmail.com>
2020-04-14 20:01:16 +0530

    ex25 created,RS

---
+++ tij2_ch12/tij2_ch12/Exercise25.java
@@ -0,0 +1,35 @@
+package tij2_ch12.tij2_ch12;
+import static custom_tools.print.Print.*;
+import java.util.*;
+
+import custom_tools.map_utility.MapUtility;
+import custom_tools.myio.TextFile;
+
+/**
+ * Create a Map<String,ArrayList<Integer>>. Use net.mindview.TextFile to open a text file and 
+ * read it in a word at a time (use "\\W+" as the second argument to the TextFile constructor). 
+ * Count the words as you read them in, and for each word in the file, record in the ArrayList<Integer> 
+ * the word count associated with that word—this is, in effect, the location in the file where that word was found. 
+ */
+public class Exercise25 {
+    static int word_count=0;
+    public static void main(String[] args) {
+        MapUtility<String,ArrayList<Integer>>mu=new MapUtility<String,ArrayList<Integer>>() {
+        };
+        LinkedList<String> wordList=new LinkedList<>();
+        wordList.addAll(new TextFile("C:/Users/aurok/OneDrive/Documents/Consider the discrete time description of a multidimensional System.docx","\\W+"));
+        HashMap<String,ArrayList<Integer>>map=mu.fill(wordList, new ArrayList<>());
+        Iterator<String> it=wordList.iterator();
+        while(it.hasNext()){
+            String s=it.next();
+            word_count++;
+            ArrayList<Integer> list=map.get(s);
+            if(list==null){
+                list=new ArrayList<>();
+            }
+            list.add(word_count);
+            map.put(s, list);
+        }
+        print(map);
+    }
+}
--- tij2_ch12/tij2_ch12_main/HoldingYourObjects.java
+++ tij2_ch12/tij2_ch12_main/HoldingYourObjects.java
@@ -9,6 +9,7 @@ import tij2_ch12.tij2_ch12.ex9.Exercise9;
 import tij2_ch12.tij2_ch12.ex15.Exercise15;
 import tij2_ch12.tij2_ch12.ex16.Exercise16;
 import tij2_ch12.tij2_ch12.ex17.Exercise17;
+import tij2_ch12.tij2_ch12.Exercise25;
 import tij2_ch12.tij2_ch12.ex1.Exercise1;
 import tij2_ch12.tij2_ch12.ex10.Exercise10;
 import tij2_ch12.tij2_ch12.ex11.Exercise11;
@@ -88,5 +89,8 @@ public class HoldingYourObjects {
 
         print("\nRunning Exercise 24 : ");
         Exercise24.main(args);
+
+        print("\nRunning Exercise 25 : ");
+        Exercise25.main(args);
     }
 }
