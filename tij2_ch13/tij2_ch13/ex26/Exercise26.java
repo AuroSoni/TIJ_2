7715f046f90b72d9d7ec02c186c760ef52fb0fba
Auro <aurokumar926@gmail.com>
2020-04-19 07:59:18 +0530

    ex26 created,RS.

---
+++ tij2_ch13/tij2_ch13/ex26/Exercise26.java
@@ -0,0 +1,14 @@
+package tij2_ch13.tij2_ch13.ex26;
+
+import java.io.FileInputStream;
+
+public class Exercise26 {
+    // Pass all exceptions to the console:   
+    public static void main(String[] args) throws Exception {     
+        // Open the file:     
+        FileInputStream file = new FileInputStream("C:/Users/aurok/OneDrive/Documents/java_projects_tij_2/tij2_ch13/tij2_ch13/ex25/Exercise31.java");     
+        // Use the file ...     
+        // Close the file:     
+        file.close();   
+    } 
+}
--- tij2_ch13/tij2_ch13_main/ErrorHandlingWithExceptions.java
+++ tij2_ch13/tij2_ch13_main/ErrorHandlingWithExceptions.java
@@ -1,4 +1,5 @@
 package tij2_ch13.tij2_ch13_main;
+
 import static custom_tools.print.Print.*;
 
 import tij2_ch13.tij2_ch13.ex4.Exercise4;
@@ -7,7 +8,8 @@ import tij2_ch13.tij2_ch13.ex6.Exercise6;
 import tij2_ch13.tij2_ch13.ex8.Exercise8;
 import tij2_ch13.tij2_ch13.ex9.Exercise9;
 import tij2_ch13.tij2_ch13.ex20.Exercise20;
-import tij2_ch13.tij2_ch13.Exercise25;
+import tij2_ch13.tij2_ch13.ex25.Exercise25;
+import tij2_ch13.tij2_ch13.ex26.Exercise26;
 import tij2_ch13.tij2_ch13.Naughty;
 import tij2_ch13.tij2_ch13.ex1.Exercise1;
 import tij2_ch13.tij2_ch13.ex10.Exercise10;
@@ -90,5 +92,11 @@ public class ErrorHandlingWithExceptions {
         print("\nRunning Exercise 25 : ");
         Exercise25.main(args);
 
+        print("\nRunning Exercise 26 : ");
+        try {
+            Exercise26.main(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
