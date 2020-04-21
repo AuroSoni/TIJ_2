cc9c6a3bd13b11eff4dce12d362a1b8a72d539de
Auro <aurokumar926@gmail.com>
2020-04-21 10:24:35 +0530

    ex2 created,RS.

--- tij2_ch14/tij2_ch14_main/Strings.java
+++ tij2_ch14/tij2_ch14_main/Strings.java
@@ -2,10 +2,14 @@ package tij2_ch14.tij2_ch14_main;
 import static custom_tools.print.Print.*;
 
 import tij2_ch14.tij2_ch14.ex1.Exercise1;
+import tij2_ch14.tij2_ch14.ex2.Exercise2;
 
 public class Strings{
     public static void main(String[] args) {
         print("Running Exercise 1 : ");
         Exercise1.main(args);
+
+        print("Running Exercise 2 : ");
+        Exercise2.main(args);
     }
 }
