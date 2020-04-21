c55b54929db8ec29ced805e037d6ed5db104695a
Auro <aurokumar926@gmail.com>
2020-04-21 18:57:55 +0530

    pending

---
+++ tij2_ch14/tij2_ch14/ex5/Exercise5.java
@@ -0,0 +1,10 @@
+package tij2_ch14.tij2_ch14.ex5;
+
+/**
+ * For each of the basic conversion types in the above table, write the most 
+ * complex formatting expression possible. That is, use all the possible format
+ * specifiers available for that conversion type. 
+ */
+public class Exercise5 {
+
+}
