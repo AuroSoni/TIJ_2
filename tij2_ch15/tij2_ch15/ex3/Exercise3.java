1c98fada206c8dcf9047998fbd3f6372ccbb0973
Auro <aurokumar926@gmail.com>
2020-04-27 09:22:35 +0530

    ex4 inc with ex3,RS.

--- tij2_ch15/tij2_ch15/ex3/Exercise3.java
+++ tij2_ch15/tij2_ch15/ex3/Exercise3.java
@@ -1,11 +1,12 @@
-package tij2_ch15.tij2_ch15;
+package tij2_ch15.tij2_ch15.ex3;
 
 import java.util.Arrays;
 import java.util.List;
+import static custom_tools.print.Print.*;
 
 abstract class Shape {
     void draw() { 
-        System.out.println(this + ".draw()"); 
+        print(this + ".draw()"); 
     }   
     abstract public String toString(); 
 } 
@@ -36,12 +37,18 @@ class Rhomboid extends Shape{
 /**
  * Add Rhomboid to Shapes.java(aka Exercise3). Create a Rhomboid, upcast it to a Shape, 
  * then downcast it back to a Rhomboid. Try downcasting to a Circle and see what happens.
+ * (Ex4)Modify the previous exercise so that it uses instanceof to check the type before performing the downcast. 
  */
 public class Exercise3 {
     public static void main(String[] args) {     
         List<Shape> shapeList = Arrays.asList(new Circle(), new Square(), new Triangle(),new Rhomboid());     
-        for(Shape shape : shapeList)  shape.draw();   
-        Circle c = (Circle) shapeList.get(3);
-        c.draw();
+        for(Shape shape : shapeList)  shape.draw();
+        Circle c;
+        if(shapeList.get(3) instanceof Circle){   
+            c = (Circle) shapeList.get(3);
+            c.draw();
+        }
+        else
+        print("Rhomboid is not a circle.");
     }
 }
